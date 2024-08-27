9df221b22cb4e7190c0affe2fbcdfd684268298b
Work-Alexey <inactive0073@gmail.com>
2024-08-27 15:04:09 +0400

    Added new locator for login page, added new tests login page

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+from selenium.webdriver.support.expected_conditions import url_contains
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert url_contains('login'), 'The URL doesn`t contain a login.'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_NAME), 'The login form isn`t present'
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REG_EMAIL), 'The registration form isn`t present'
